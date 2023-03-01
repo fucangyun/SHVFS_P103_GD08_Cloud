c67cccdf5417a4eb364f8fa942440a50b71cb8d7
fucangyun <954468261@qq.com>
2023-03-01 19:42:55 +0800

    update

--- Assets/SHVFS_P103/Scripts/IngredientSpawnerComponent.cs
+++ Assets/SHVFS_P103/Scripts/IngredientSpawnerComponent.cs
@@ -3,8 +3,33 @@ using UnityEngine;
 
 public class IngredientSpawnerComponent:InteractableComponentBase
 {
+    public bool IsOnDesk = false;
+    public bool IsOnHand = false;
+
+    public GameObject Food;
+    public Transform SpawnPosition;
+    public Transform handposition;
+    public GameObject Foodspawn;
     public override void Interact()
     {
-        Debug.Log("Food Spawn!");
+
+        if (IsOnDesk == false && IsOnHand == false)
+        {
+            Foodspawn = Instantiate(Food, SpawnPosition.transform);
+            IsOnDesk = true;
+
+        }
+        else if (IsOnDesk == false && IsOnHand == true)
+        {
+            Foodspawn.transform.SetParent(SpawnPosition);
+            IsOnDesk = true;
+            IsOnHand = false;
+        }
+        else if (IsOnDesk == true && IsOnHand == false)
+        {
+            Foodspawn.transform.SetParent(handposition);
+            IsOnDesk = false;
+            IsOnHand = true;
+        }
     }
 }
--- Assets/SHVFS_P103/Scripts/InteractableComponent.cs
+++ Assets/SHVFS_P103/Scripts/InteractableComponent.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InteractableComponent : MonoBehaviour
+public class InteractableComponent : InteractableComponentBase
 {
 
-    //public abstract void Interact();
+    public override void Interact()
+    {
+
+    }
 }
